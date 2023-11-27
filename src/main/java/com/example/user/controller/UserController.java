760d63d681b3ea17cc149d9f388e76d915168960
bongdukk <pond9762296@naer.com>
2023-11-27 18:16:30 +0900

    commit

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
