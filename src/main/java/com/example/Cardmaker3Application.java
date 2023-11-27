4e83d5415876a8fb1c7f26102e9bb2d68ea0244d
bongdukk <pond9762296@naer.com>
2023-11-27 17:20:50 +0900

    commit

---
+++ src/main/java/com/example/Cardmaker3Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cardmaker3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cardmaker3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cardmaker3/Cardmaker3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardmaker3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cardmaker3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
